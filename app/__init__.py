f3875d963cf9768acc03d3bf7d1b3a882245e43a
Marst <marstaiwebstudio@gmail.com>
2026-05-24 23:00:29 +0900

    add CORS

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS
 from dotenv import load_dotenv
 import os
 
@@ -12,20 +13,17 @@ jwt = JWTManager()
 def create_app():
     app = Flask(__name__)
 
-    # 설정
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///school.db')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY', 'dev-secret-key')
 
-    # 확장 초기화
     db.init_app(app)
     jwt.init_app(app)
+    CORS(app)  # 추가!
 
-    # 라우트 등록
     from app.routes import main
     app.register_blueprint(main)
 
-    # DB 테이블 생성
     with app.app_context():
         from app import models
         db.create_all()
